0dbc4b96d50e0448609d199b928826c7dea1a54e
kiminn <808010x@gmail.com>
2023-09-07 22:28:11 +0900

    feat: snspage모달창구현, 목데이터 받아와야함

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { route } from "./routes/route" 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import GlobalStyles from "./styles/global.style";
 =======
 >>>>>>> 77946d4 (reabase : mreged main)
+=======
+>>>>>>> be465a6 (feat: snspage모달창구현, 목데이터 받아와야함)
 
 function App() {
   return (
     <>
 <<<<<<< HEAD
+<<<<<<< HEAD
       <GlobalStyles />
 =======
 >>>>>>> 77946d4 (reabase : mreged main)
+=======
+>>>>>>> be465a6 (feat: snspage모달창구현, 목데이터 받아와야함)
       <RouterProvider router={route} />
     </>
   )
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 import {
     createBrowserRouter,
 <<<<<<< HEAD
+<<<<<<< HEAD
 } from "react-router-dom";
 import SnsPage from "../pages/sns";
 import MainPage from "../pages/login";
@@ -9,11 +10,17 @@ import MainPage from "../pages/login";
 } from "react-router-dom";
 import SnsPage from "../pages";
 >>>>>>> 77946d4 (reabase : mreged main)
+=======
+    RouterProvider,
+} from "react-router-dom";
+import SnsPage from "../pages";
+>>>>>>> be465a6 (feat: snspage모달창구현, 목데이터 받아와야함)
 
 
 export const route = createBrowserRouter ([
     {
       path: "/",
+<<<<<<< HEAD
 <<<<<<< HEAD
       element: <MainPage />,
     },
@@ -23,5 +30,8 @@ export const route = createBrowserRouter ([
 =======
       element: <SnsPage />,
 >>>>>>> 77946d4 (reabase : mreged main)
+=======
+      element: <SnsPage />,
+>>>>>>> be465a6 (feat: snspage모달창구현, 목데이터 받아와야함)
     },
 ])
